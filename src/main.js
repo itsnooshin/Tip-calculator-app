f67ae4b24d8532e978d24a9f5354355469565534
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-18 13:09:51 +0330

    add reset button on javascript file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ const total = document.querySelector('.total-amount');
 const tipPer = document.querySelectorAll('.btn-tip');
 const tipAmount = document.querySelector('.tip-amount');
 const tipnum = document.querySelectorAll('.tip-but');
-const btnReset = document.querySelectorAll('.btn-end-reset');
+const btnReset = document.querySelector('.btn-end-reset');
+const prevDiv1 = document.querySelector('.prevDivIocn');
 
 let prevDiv = null;
 const numBill = function () {
@@ -15,8 +16,10 @@ const numpeople = function () {
   let num1 = Number(numel.value);
   calcuate();
 };
-let selectNumber = null;
-const btn = function (numBtn) {
+bill.addEventListener('input', numBill);
+numel.addEventListener('input', numpeople);
+
+tipPer.forEach(function (numBtn) {
   numBtn.addEventListener('click', function (e) {
     e.preventDefault();
     if (numBtn === e.target) {
@@ -30,30 +33,32 @@ const btn = function (numBtn) {
       let btnNumi = parseInt(numBtn.textContent);
       selectNumber = btnNumi;
     }
+    calcuate();
   });
-};
+});
 
-const calcuate = function (e) {
+let selectNumber = null;
+// calcuate the total and the tip amount
+const calcuate = function () {
   const first = Number(bill.value);
   const second = Number(numel.value);
   const tip = selectNumber;
-  const tipAmount1 = (first * tip) / 100;
-  const er = tipAmount1 / second;
-  const amount = er;
-  let totalamount = ((first + tipAmount1) / second).toFixed(2);
-
-  total.textContent = totalamount;
-  tipAmount.textContent = amount;
+  const tipAmount1 = parseFloat((first * tip) / 100);
+  const er = parseFloat(tipAmount1 / second);
+  const amount = er.toFixed(2);
+  let totalamount = parseFloat(((first + tipAmount1) / second).toFixed(2));
+  if (totalamount !== Infinity && amount !== Infinity) {
+    total.textContent = totalamount;
+    tipAmount.textContent = amount;
+  }
 };
 
-const sum = function () {
-  tipPer.forEach(function (num) {
-    btn(num);
-  });
-};
-bill.addEventListener('input', numBill);
-numel.addEventListener('input', numpeople);
-
-
-
-sum();
+// reset button
+btnReset.addEventListener('click', function () {
+  bill.value = '';
+  numel.value = '';
+  total.textContent = '0.00';
+  tipAmount.textContent = '0.00';
+  prevDiv.style.backgroundColor = '';
+  prevDiv.style.color = '#ffff';
+});
